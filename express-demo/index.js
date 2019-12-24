d07e470bd038bc17d02dd1dc7194c8fa160dc2a6
sandunsameera <sandunsameera25@gmail.com>
2019-12-24 15:03:02 +0530

    First get method

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,7 +2,11 @@ const express =require('express');
  const app = express();
 
  app.get('/',(req,res)=>{
-     res.send("Helo node");
+     res.send("Hello node");
  });
+
+ app.get('/api/course',(req,res)=>{
+     res.send([1,2,3])
+ })
 
  app.listen(3000,()=>console.log("Post 3000 is listinig"));
